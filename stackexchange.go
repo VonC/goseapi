fc04021ddfc1ea01fc0a2e959dbfdc2ae4a458a5
Ross Light <ross@zombiezen.com>
2013-01-15 14:01:09 -0800

    Add Root constant (1:980630f6cb19)

--- stackexchange.go
+++ stackexchange.go
@@ -1,2 +1,5 @@
 // Package stackexchange provides access to the Stack Exchange 2.0 API.
 package stackexchange
+
+// Root is the Stack Exchange API endpoint.
+const Root = "https://api.stackexchange.com/2.1"
